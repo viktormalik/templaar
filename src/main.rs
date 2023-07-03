687ac5d1f31e5537f2f967a820ef2527d7f8264c
Viktor Malik <viktor.malik@gmail.com>
2023-07-03 11:21:56 +0200

    Add 'new' command
    Opens an editor and saves the created file as .<name>.aar.
    Default name is "templ".

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use clap::{Parser, Subcommand};
+use std::{
+    env, error,
+    io::{self, Write},
+    process,
+};
+
+#[derive(Debug, Parser)]
+#[clap(name = "templaar")]
+struct Templaar {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    New,
+}
+
+fn new() -> Result<(), Box<dyn error::Error>> {
+    let mut templ_name = String::new();
+    print!("Enter template name (default 'templ'): ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut templ_name)?;
+
+    if templ_name.trim().is_empty() {
+        templ_name = "templ".to_string();
+    }
+
+    let editor = env::var("EDITOR")?;
+    let templ_file = env::current_dir()?.join(format!(".{templ_name}.aar"));
+    process::Command::new(editor).arg(&templ_file).status()?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let templaar = Templaar::parse();
+
+    if let Err(e) = match templaar.command {
+        Command::New => new(),
+    } {
+        eprintln!("Error: {e}");
+        process::exit(1);
+    }
 }
