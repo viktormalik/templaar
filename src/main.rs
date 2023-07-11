dfe05344fcb5ec707e99584425a7a1035eddaa9c
Viktor Malik <viktor.malik@gmail.com>
2023-07-11 17:41:30 +0200

    Add 'take' command
    Finds a template (a file called ".<name>.aar") starting from the current
    directory and following the parent directories. Then, creates a new file
    called "<name>" in the current directory with the contents of the
    template and opens the new file with $EDITOR.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use clap::{Parser, Subcommand};
 use std::{
     env, error,
-    io::{self, Write},
+    ffi::OsStr,
+    fmt, fs,
+    io::{self, Read, Write},
+    path::{Path, PathBuf},
     process,
 };
 
@@ -15,6 +18,19 @@ struct Templaar {
 #[derive(Subcommand, Debug)]
 enum Command {
     New { name: Option<String> },
+    Take,
+}
+
+/// Templaar errors
+#[derive(Debug, Clone)]
+struct NoTemplateFound;
+
+impl error::Error for NoTemplateFound {}
+
+impl fmt::Display for NoTemplateFound {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "No template found in the current or parent directories")
+    }
 }
 
 fn new(name: &Option<String>) -> Result<(), Box<dyn error::Error>> {
@@ -41,11 +57,57 @@ fn new(name: &Option<String>) -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
+/// Searches for a file with the .aar extension in `dir`
+fn find_templ_in_dir(dir: &Path) -> Result<Option<PathBuf>, io::Error> {
+    match fs::read_dir(dir)?.find(|f| match f {
+        Ok(file) => file.path().extension() == Some(OsStr::new("aar")),
+        Err(_) => false,
+    }) {
+        Some(f) => f.map(|file| Some(file.path())),
+        None => Ok(None),
+    }
+}
+
+/// Searches for a template, starting from the current directory and recursively descending into
+/// its parents, until any template is found.
+fn find_templ() -> Result<Option<PathBuf>, io::Error> {
+    let mut dir = env::current_dir()?;
+    loop {
+        match find_templ_in_dir(&dir)? {
+            Some(file) => return Ok(Some(dir.join(&file))),
+            None => match dir.parent() {
+                Some(parent) => dir = parent.to_path_buf(),
+                None => return Ok(None),
+            },
+        }
+    }
+}
+
+fn take() -> Result<(), Box<dyn error::Error>> {
+    // Find and read the template
+    let templ_file = find_templ()?.ok_or(NoTemplateFound)?;
+    let mut templ = String::new();
+    fs::File::open(&templ_file)?.read_to_string(&mut templ)?;
+
+    // default filename is the template name without the leading '.' and file extension
+    let filename: String = templ_file.file_stem().unwrap().to_str().unwrap()[1..].to_string();
+    let file = env::current_dir()?.join(filename);
+
+    // Write template contents to file and open it in EDITOR
+    fs::File::create(&file)?.write_all(templ.as_bytes())?;
+
+    let editor = env::var("EDITOR")?;
+    process::Command::new(editor).arg(file).status()?;
+
+    Ok(())
+}
+
 fn main() {
     let templaar = Templaar::parse();
 
     if let Err(e) = match templaar.command {
         Command::New { name } => new(&name),
+        Command::Take => take(),
     } {
         eprintln!("Error: {e}");
         process::exit(1);
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,12 @@
 use assert_cmd::Command;
 use serial_test::serial;
-use std::{env, error::Error, fs, path::Path};
+use std::{
+    env,
+    error::Error,
+    fs,
+    io::{Read, Write},
+    path::Path,
+};
 
 fn set_editor(editor: &str) {
     env::set_var("EDITOR", editor);
@@ -78,6 +84,66 @@ fn test_invalid_editor() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("templaar")?;
     cmd.arg("new").arg("note");
     cmd.assert().failure();
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_take_same_dir() -> Result<(), Box<dyn Error>> {
+    set_default_editor();
+
+    let dir = Path::new("testdir");
+    fs::create_dir(dir)?;
+    let templ_path = dir.join(".templ.aar");
+    fs::File::create(&templ_path)?.write_all(b"Template")?;
+
+    let cwd = env::current_dir()?;
+    env::set_current_dir(dir)?;
+
+    let mut cmd = Command::cargo_bin("templaar")?;
+    cmd.arg("take");
+    cmd.assert().success();
+
+    let file_path = Path::new("templ");
+    let mut contents = String::new();
+    assert!(file_path.exists());
+    fs::File::open(&file_path)?.read_to_string(&mut contents)?;
+    assert_eq!(contents, "Template");
+
+    env::set_current_dir(cwd)?;
+    fs::remove_dir_all(&dir)?;
+
+    Ok(())
+}
+
+#[test]
+#[serial]
+fn test_take_subdir() -> Result<(), Box<dyn Error>> {
+    set_default_editor();
+
+    let templ_dir = Path::new("testdir");
+    let file_dir = templ_dir.join("subdir");
+    fs::create_dir_all(&file_dir)?;
+
+    let templ_path = templ_dir.join(".templ.aar");
+    fs::File::create(&templ_path)?.write_all(b"Template")?;
+
+    let cwd = env::current_dir()?;
+    env::set_current_dir(file_dir)?;
+
+    let mut cmd = Command::cargo_bin("templaar")?;
+    cmd.arg("take");
+    cmd.assert().success();
+
+    let file_path = Path::new("templ");
+    let mut contents = String::new();
+    assert!(file_path.exists());
+    fs::File::open(&file_path)?.read_to_string(&mut contents)?;
+    assert_eq!(contents, "Template");
+
+    env::set_current_dir(cwd)?;
+    fs::remove_dir_all(&templ_dir)?;
 
     Ok(())
 }
